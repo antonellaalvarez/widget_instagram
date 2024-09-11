3baf9c61c18e015daeb3f656a74c9d402ecef4b4
antonellaalvarez <150460397+antonellaalvarez@users.noreply.github.com>
2024-09-11 10:45:35 -0300

    Actualizacion

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 document.addEventListener('DOMContentLoaded', function() {
-    fetch('https://rss.app/feeds/0Az8vJ69zfkLHfOe.xml')  // URL del feed RSS
-    .then(response => response.text())
-    .then(data => {
-        // Parsear el feed RSS y extraer las imágenes
-        const parser = new DOMParser();
-        const xmlDoc = parser.parseFromString(data, "text/xml");
-        const items = xmlDoc.getElementsByTagName('item');
-        const widget = document.getElementById('instagram-widget');
+    function fetchAndUpdateFeed() {
+        fetch('https://rss.app/feeds/0Az8vJ69zfkLHfOe.xml')  // URL del feed RSS
+        .then(response => response.text())
+        .then(data => {
+            // Limpiar el contenido actual del widget
+            const widget = document.getElementById('instagram-widget');
+            widget.innerHTML = '';
 
-        Array.from(items).forEach(item => {
-            const img = document.createElement('img');
-            const mediaContent = item.getElementsByTagName('media:content')[0];
-            if (mediaContent) {
-                const imageUrl = mediaContent.getAttribute('url');
-                img.src = imageUrl;
-                img.alt = item.getElementsByTagName('title')[0].textContent;
-                widget.appendChild(img);
-            }
-        });
-    })
-    .catch(error => console.error('Error fetching RSS feed:', error));
+            // Parsear el feed RSS y extraer las imágenes
+            const parser = new DOMParser();
+            const xmlDoc = parser.parseFromString(data, "text/xml");
+            const items = xmlDoc.getElementsByTagName('item');
+
+            Array.from(items).forEach(item => {
+                const img = document.createElement('img');
+                const mediaContent = item.getElementsByTagName('media:content')[0];
+                if (mediaContent) {
+                    const imageUrl = mediaContent.getAttribute('url');
+                    img.src = imageUrl;
+                    img.alt = item.getElementsByTagName('title')[0].textContent;
+                    widget.appendChild(img);
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching RSS feed:', error));
+    }
+
+    // Fetch and update feed initially
+    fetchAndUpdateFeed();
+
+    // Set an interval to fetch and update feed every 30 minutes
+    setInterval(fetchAndUpdateFeed, 30 * 60 * 1000);  // 30 minutes in milliseconds
 });
